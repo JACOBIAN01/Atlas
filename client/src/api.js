cbe165ddacacbeb17aa6b9a95f1ec168442a84d1
JACOBIAN01 <subhadeepghorai23@gmail.com>
2025-11-16 14:33:04 +0530

    Config

--- client/src/api.js
+++ client/src/api.js
@@ -14,19 +14,3 @@ export const SubmitModules = async (data) => {
   }).then((res) => res.json());
 };
 
-// export const GAS_URL =
-//   "https://script.google.com/macros/s/AKfycbz5Dkhrv2MpGF8i01NA3JJc7kN91_1C9lQZ7jp7Tv0LgrjD2ggw7FYng1HoxA13HMLO/exec";
-
-// export const VerifyEmail = async (email) => {
-//   return fetch(`${GAS_URL}?email=${email}`).then((res) => res.json());
-// };
-
-// export const SubmitModules = async (data) => {
-//   return fetch(GAS_URL, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(data),
-//   }).then((res) => res.json());
-// };
