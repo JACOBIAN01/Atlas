70942f1ed899c00b746bf676c3b59c19114bbb76
JACOBIAN01 <subhadeepghorai23@gmail.com>
2025-11-17 18:49:20 +0530

    Update

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,12 @@
+// export const VerifyEmail = async (email) => {
+//   return fetch(
+//     `https://atlas-3-pl8c.onrender.com/verify-email?email=${email}`
+//   ).then((res) => res.json());
+// };
+
 export const VerifyEmail = async (email) => {
   return fetch(
-    `https://atlas-3-pl8c.onrender.com/verify-email?email=${email}`
+    `https://script.google.com/macros/s/AKfycbz5Dkhrv2MpGF8i01NA3JJc7kN91_1C9lQZ7jp7Tv0LgrjD2ggw7FYng1HoxA13HMLO/exec?email=${email}`
   ).then((res) => res.json());
 };
 
@@ -13,4 +19,3 @@ export const SubmitModules = async (data) => {
     body: JSON.stringify(data),
   }).then((res) => res.json());
 };
-
