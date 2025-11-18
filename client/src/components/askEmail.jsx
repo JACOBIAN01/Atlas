6eb81ecd7ae564abb6476fc75ccb5a55d2f146cd
JACOBIAN01 <subhadeepghorai23@gmail.com>
2025-11-19 00:29:59 +0530

    Update

--- client/src/components/askEmail.jsx
+++ client/src/components/askEmail.jsx
@@ -85,7 +85,7 @@ function AskEmail({ setTeacherData }) {
         </button>
         {/* Version */}
         <p className="text-xs text-gray-500 mt-4 text-center bg-gray-100 px-3 py-1 rounded-full inline-block mx-auto">
-          V 0.0.2
+          V 0.0.3
         </p>
 
         {/* Result Box */}
